56a25abfb40a96a5e844a2eb1db25d08b7392c15
Artyom Tetyukhin <51746822+arttet@users.noreply.github.com>
2026-05-24 04:58:20 +0400

    feat: add Nix dotfiles config (#42)

---
+++ nixos/home.nix
@@ -0,0 +1,46 @@
+{
+  config,
+  lib,
+  dotfilesRoot,
+  ...
+}:
+let
+  link = config.lib.file.mkOutOfStoreSymlink;
+  dotfileLinks = [
+    ".config/alacritty"
+    ".config/bash"
+    ".config/fastfetch"
+    ".config/git"
+    ".config/lazygit"
+    ".config/nushell"
+    ".config/nvim"
+    ".config/shell"
+    ".config/starship"
+    ".config/tmux"
+    ".config/wezterm"
+    ".config/yazi"
+    ".config/zsh"
+    ".zshenv"
+  ];
+  missingDotfileLinks = builtins.filter (target: !(builtins.pathExists "${dotfilesRoot}/${target}")) dotfileLinks;
+  linkFile =
+    target:
+    {
+      source = link "${dotfilesRoot}/${target}";
+    };
+in
+{
+  assertions = [
+    {
+      assertion = missingDotfileLinks == [ ];
+      message = "dotfilesRoot is missing dotfile link targets: ${lib.concatStringsSep ", " missingDotfileLinks}";
+    }
+  ];
+
+  home.file = builtins.listToAttrs (
+    map (target: {
+      name = target;
+      value = linkFile target;
+    }) dotfileLinks
+  );
+}
